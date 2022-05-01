a07b69b7bfa2569c02fb02b0a050da81e2b26f36
Andres Mejia <73647620+Afmc07@users.noreply.github.com>
2022-05-01 10:28:41 -0300

    Spacing changes

--- lib/wubot/football.ex
+++ lib/wubot/football.ex
@@ -6,11 +6,14 @@ defmodule Football do
       "leagues" ->
         league_list()
 
-      "help" ->
+      "help" -> 
         help()
-      "seasons" -> league_seasons_list(parameter)
+        
+      "seasons" -> 
+        league_seasons_list(parameter)
 
-      _ -> get_season(command, parameter)
+      _ -> 
+        get_season(command, parameter)
     end
   end
 
@@ -24,8 +27,7 @@ defmodule Football do
   end
 
   defp help do
-    "**Football Command List**\n\n" <>
-      Enum.join(["`leagues`", "`seasons [leagueId]`", "`[leagueId] [year]`"], "\n")
+    "**Football Command List**\n\n" <> Enum.join(["`leagues`", "`seasons [leagueId]`", "`[leagueId] [year]`"], "\n")
   end
 
   defp league_list do
